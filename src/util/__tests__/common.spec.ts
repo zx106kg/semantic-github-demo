44c23e62a8ed20d387ea7452e8ae77ce7262584d
zx <429792584@qq.com>
2023-08-10 16:07:42 +0800

    feat: 增加工具方法
    添加sub方法

--- src/util/__tests__/common.spec.ts
+++ src/util/__tests__/common.spec.ts
@@ -1,7 +1,13 @@
-import { add } from "../common";
+import { add, sub } from "../common";
 
 describe("add", function () {
   it("should add", function () {
     expect(add(1, 2)).toEqual(3);
+  });
+});
+
+describe("sub", function () {
+  it("should sub", function () {
+    expect(sub(1, 2)).toEqual(3);
   });
 });
